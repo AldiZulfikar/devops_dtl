04600ab68ec06bf9fd26ed4f76daf0e0f1b4faa3
AldiZulfikar <aldizulfikar04@gmail.com>
2022-11-04 11:34:46 +0700

    testing

---
+++ app/tests/test_home.py
@@ -0,0 +1,21 @@
+import pytest
+
+from main import app
+
+# --------
+# Fixtures
+# --------
+
+
+@pytest.fixture()
+def client():
+    return app.test_client()
+
+def test_home(client):
+    resp = client.get('/')
+    assert resp.status_code == 200
+    assert b"Lets Predict Source Code of Your Software!" in resp.data
+
+def test_home_bad_http_method(client):
+    resp = client.post('/')
+    assert resp.status_code == 405
